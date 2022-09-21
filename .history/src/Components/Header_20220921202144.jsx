3914a22f0c54e878ab83ebea30c3d46d80a3d5d8
shikubro90 <shikubro90@gmail.com>
2022-09-21 20:44:29 +0600

    update

--- .history/src/Components/Header_20220921202144.jsx
+++ .history/src/Components/Header_20220921202144.jsx
@@ -38,6 +38,7 @@ const Header = () => {
     }
   }
 
+  console.log(user.email)
 
   return (
     <div className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
@@ -55,9 +56,7 @@ const Header = () => {
 
         <div className="flex justify-center items-center">
           <motion.ul 
-          initial={{opacity: 0, x: 200}}
-          animate={{opacity: 1, x: 0}}
-          exit={{opacity: 0, x: 200}}
+          initial={{opacity: 0}}
           className="flex items-center justify-center gap-8 ml-auto">
             <li className="text-base text-textColor cursor-pointer hover:text-gray-600 duration-100 ease-in-out">
               Home
@@ -117,44 +116,7 @@ const Header = () => {
       </div>
 
       {/* Mobile */}
-      <div className="flex items-center justify-between md:hidden w-full h-full">
-        <Link to={'/'}>
-          <motion.div
-            whileTap={{ scale: 0.6 }}
-            className="flex items-center gap-2"
-          >
-            <img src={logo} alt="logo" className="w-10 object-cover" />
-            <p className="text-gray-800 text-xl font-bold">City</p>
-          </motion.div>
-        </Link>
-
-        <div className="relative">
-            
-            <motion.img
-              whileTap={{ scale: 0.6 }}
-              src={user ? user.photoURL : Avatar}
-              alt=""
-              className="w-6 h-6 min-w-[40px] min-h-[40px] ml-5 cursor-pointer drop-shadow-xl rounded-full"
-              onClick={login}
-            />
-          
-            {isMenu && (
-              <motion.div 
-              initial={{opacity: 0, scale: 0.6}} 
-              animate={{opacity: 1, scale: 1}} 
-              exit={{opacity: 0, scale: 0.6}} 
-              className='w-40 shadow-xl absolute bg-gray-50 right-0 top-12 rounded-lg flex flex-col'>
-              {user && user.email === 'shikubro90@gmail.com' && (
-              <Link to="/create">
-              <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item <GrAdd/></p>
-              </Link>
-              )}
-              <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>Logout <FiLogOut/></p>
-            </motion.div>
-            )}
-
-          </div>
-      </div>
+      <div className="flex md:hidden w-full h-full bg-blue-400">Mobile</div>
     </div>
   )
 }
