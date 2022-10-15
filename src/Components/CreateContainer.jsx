5b66ce9e3c45025e943372be5ae36815f97aa365
wp-dev-ruhul <wp.dev.ruhul@gmail.com>
2022-10-15 06:55:47 +0600

    update

--- src/Components/CreateContainer.jsx
+++ src/Components/CreateContainer.jsx
@@ -1,38 +1,162 @@
-import React from 'react'
-import { useState } from 'react'
-import { motion } from 'framer-motion'
+import React from "react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import {
+  MdFastfood,
+  MdCloudUpload,
+  MdDelete,
+  MdFoodBank,
+  MdAttachMoney,
+} from "react-icons/md";
+import { categories } from "../utils/Data";
+import Loader from "./Loader";
 
 const CreateContainer = () => {
-  const [title, setTitle] = useState('')
-  const [calories, setCalories] = useState('')
-  const [price, setPrice] = useState('')
-  const [category, setCategory] = useState(null)
-  const [field, setField] = useState(true)
-  const [alertStatus, setAlertStatus] = useState('danger')
-  const [msg, setMsg] = useState("message")
-  const [imageAsset, setImageAssets] = useState(null)
-  const [isLoading, setImageLoading] = useState(false)
+  const [title, setTitle] = useState("");
+  const [calories, setCalories] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState(null);
+  const [field, setField] = useState(false);
+  const [alertStatus, setAlertStatus] = useState("danger");
+  const [msg, setMsg] = useState("message");
+  const [imageAsset, setImageAssets] = useState(null);
+  const [isLoading, setImageLoading] = useState(false);
 
+  const uploadImage = () => {};
+  const deleteImage = () => {};
+  const saveDetails = () => {};
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
-      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex items-center justify-center">
+      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
         {field && (
           <motion.p
-            initial={{opacity:0}}
-            animate={{opacity:1}}
-            exit={{opacity:0}}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
             className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
-              alertStatus === 'danger'
-                ? 'bg-red-400 text-red-800'
-                : 'bg-emerald-400 text-emerald-800'
+              alertStatus === "danger"
+                ? "bg-red-400 text-red-800"
+                : "bg-emerald-400 text-emerald-800"
             }`}
           >
             {msg}
           </motion.p>
         )}
+
+        <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+          <MdFastfood className="text-gray-700 text-2xl" />
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Give me a title"
+            className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+          />
+        </div>
+
+        <div className="w-full">
+          <select
+            name=""
+            id=""
+            onChange={(e) => setCategory(e.target.value)}
+            className="outline-none w-full border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+          >
+            <option value="other" className="bg-white">
+              Select Category
+            </option>
+
+            {categories &&
+              categories.map((item) => {
+                return (
+                  <option
+                    key={item.key}
+                    value={item.urlParamName}
+                    className="text-base border-0 outline-none capitalize bg-white text-headingColor"
+                  >
+                    {item.name}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
+
+        <div className="flex justify-center group items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {!imageAsset ? (
+                <>
+                  <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                    <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+                      <MdCloudUpload className="text-gray-300 text-3xl hover:text-gray-700" />
+                      <p className="text-gray-500 hover:text-gray-700">
+                        Click here to upload
+                      </p>
+                    </div>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      name="uploadImage"
+                      onChange={uploadImage}
+                      className="w-0 h-0"
+                    />
+                  </label>
+                </>
+              ) : (
+                <>
+                  <div className="relative h-full">
+                    <img src={imageAsset} alt="" />
+                    <button
+                      type="button"
+                      className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out"
+                      onClick={deleteImage}
+                    >
+                      <MdDelete className="text-white" />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </div>
+        <div className="w-full flex flex-col md:flex-row items-center gap-3">
+          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+            <MdFoodBank className="text-gray-700 text-2xl" />
+            <input
+              type="text"
+              required
+              value={calories}
+              onChange={(e) => setCalories(e.target.value)}
+              placeholder="Calories"
+              className="w-full text-lg bg-transparent outline-none text-textColor border-none placeholder:text-gray-400"
+            />
+          </div>
+          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+            <MdAttachMoney className="text-gray-700 text-2xl" />
+            <input
+              type="number"
+              required
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              placeholder="Price"
+              className="w-full text-lg bg-transparent outline-none text-textColor border-none placeholder:text-gray-400"
+            />
+          </div>
+        </div>
+        <div className="flex items-center w-full">
+          <button
+            type="button"
+            className="px-12 py-2 ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 rounded-lg text-lg text-white font-semibold"
+            onClick={saveDetails}
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateContainer
+export default CreateContainer;
